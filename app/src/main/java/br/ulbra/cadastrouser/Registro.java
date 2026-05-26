6653b3cd81c2bf5298b5dab106ea7f6774755e7d
NicolauDoMau <nicolas.fernandes02072009@gmail.com>
2026-05-25 21:39:20 -0300

    Cadastro finalizado

---
+++ app/src/main/java/br/ulbra/cadastrouser/Registro.java
@@ -0,0 +1,31 @@
+package br.ulbra.cadastrouser;
+
+public class Registro {
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+
+    // Construtor: usado para criar um novo objeto Registro com os dados iniciais
+    public Registro(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+
+    // Métodos "get": usados para acessar os dados armazenados no objeto
+    public String getNome() {
+        return nome;
+    }
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+
+    public String getTelefone() {
+        return telefone;
+    }
+}
